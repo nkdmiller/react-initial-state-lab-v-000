3d03d00ab9b8aac09a308bdeea6465259b736a04
Nate Miller <natekmiller96@gmail.com>
2018-11-01 19:38:24 +0000

    automatically backed up by learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,25 @@
 // your ImageSlider code here!
+import React, { Component } from 'react';
+ export default class Cell extends Component {
+   constructor(props) {
+    super(props)
+      this.state = {
+      color: props.value
+      }
+  }
+     stateColor = () => (
+      this.setState({color: '#333'})
+    )
+     backgroundColor = () => (
+      this.state.color
+    )
+   render() {
+    return (
+      <div
+        className="cell"
+        style={{backgroundColor: this.backgroundColor()}}
+        onClick={this.stateColor}>
+      </div>
+    )
+  }
+ }
